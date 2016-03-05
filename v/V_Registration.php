d829c8ec73f983198b9a9fba7242b01369902e74
vladimur <admin@admin.com>
2016-03-05 19:41:35 +0600

    sd

--- v/V_Registration.php
+++ v/V_Registration.php
@@ -3,12 +3,12 @@
 
     <form id = "registr_form" name = "registration" method="post">
 
-        <div><div class = "reg_form_name">Имя *</div>               <div class = "reg_form_input"><input name = "first_name"  placeholder = "Введите своё имя" type="text" required></div></div>
-        <div><div class = "reg_form_name">Фамилия</div>             <div class = "reg_form_input"><input name = "last_name"   placeholder = "Введите свою фамилию" type="text"></div></div>
-        <div><div class = "reg_form_name">E-mail *</div>            <div class = "reg_form_input"><input name = "e-mail"      placeholder = "Введите свой e-mail" type="email" required></div></div>
-        <div><div class = "reg_form_name">Логин *</div>             <div class = "reg_form_input"><input name = "login"       placeholder = "Введите логин" type="text" required></div></div>
-        <div><div class = "reg_form_name">Пароль *</div>            <div class = "reg_form_input"><input name = "password"    placeholder = "Введите пароль" type="password" required id = "password1"></div></div>
-        <div><div class = "reg_form_name">Подтвердите пароль *</div><div class = "reg_form_input"><input name = "re-password" placeholder = "Подтвердите пароль" type="password" required id = "password2"></div></div>
+        <div><div class = "reg_form_name">Имя *</div>               <div class = "reg_form_input"><input name = "first_name"  placeholder = "Введите своё имя"     type="text"     required></div>                 </div>
+        <div><div class = "reg_form_name">Фамилия</div>             <div class = "reg_form_input"><input name = "last_name"   placeholder = "Введите свою фамилию" type="text"             ></div>                 </div>
+        <div><div class = "reg_form_name">E-mail *</div>            <div class = "reg_form_input"><input name = "e-mail"      placeholder = "Введите свой e-mail"  type="email"    required></div>                 </div>
+        <div><div class = "reg_form_name">Логин *</div>             <div class = "reg_form_input"><input name = "login"       placeholder = "Введите логин"        type="text"     required></div>                 </div>
+        <div><div class = "reg_form_name">Пароль *</div>            <div class = "reg_form_input"><input name = "password"    placeholder = "Введите пароль"       type="password" required id = "password1"></div></div>
+        <div><div class = "reg_form_name">Подтвердите пароль *</div><div class = "reg_form_input"><input name = "re-password" placeholder = "Подтвердите пароль"   type="password" required id = "password2"></div></div>
 
         <div id = "error_pass"></div>
 
