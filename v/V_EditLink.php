d206d1c81f8e4097948765d5f04427ec72dc59b1
vladimur <admin@admin.com>
2016-03-05 16:08:32 +0600

    sd

--- v/V_EditLink.php
+++ v/V_EditLink.php
@@ -1,27 +1,25 @@
-<div>
+<div id = "v_editLinks">
     <p>Hello, this is the edit link page of linkStorage. Here you can edit your link.</p>
+
+    <form id = "editLink" name = "editLink" method="post">
+
+        <div><div class = "editLink_name">Name</div>       <div class = "reg_form_input"><input name = "name"        placeholder = "Введите название ссылки" value="<?=$link['name']?>"        type="text" required></div></div>
+        <div><div class = "editLink_name">Address</div>    <div class = "reg_form_input"><input name = "address"     placeholder = "Введите адрес ссылки"    value="<?=$link['address']?>"     type="text" required></div></div>
+        <div><div class = "editLink_name">Description</div><div class = "reg_form_input"><input name = "description" placeholder = "Введите описание ссылки" value="<?=$link['description']?>" type="text"         ></div></div>
+
+        <div>
+            <div class = "editLink_name" id = "crutch">Status</div>
+
+            <div class = "reg_form_input">
+
+                <input type="radio" id = "private" name="status" value="private" <?php if ($link['status'] == 'private') {echo 'checked'; } ?> > <label for="private">private</label>
+                <input type="radio" id = "public"  name="status" value="public"  <?php if ($link['status'] == 'public')  {echo 'checked'; } ?> > <label for="public">public</label>
+
+            </div>
+        </div>
+
+
+        <div id = "button"><button type="submit">Save</button></div>
+
+    </form>
 </div>
-
-<?php
-
-echo "<table>";
-
-    foreach ($link as $key => $value) {
-
-    echo "<tr>";
-
-        if ($key != 'status' && $key != 'id') {
-        echo "<td>" . $key . "</td><td>&nbsp;&nbsp;&nbsp;</td><td><input type='text' value ='" . $value, "' ></td>";
-        }
-
-        echo "</tr>";
-
-    }
-
-    echo "<td rowspan='2'>" . '<br><a href="">Save</a>' . "</td>";
-
-    echo "<br>";
-
-    echo "</table>";
-
-?>
